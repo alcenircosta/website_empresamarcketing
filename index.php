b9b620e98a505d7f099b1e58d70e514a6f910437
Alcenir Costa <alcenir.1994@gmail.com>
2020-04-16 10:14:20 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Projeto 10</title>
+	<link rel="stylesheet" href="_css/style.css"/>
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap" rel="stylesheet"/>
+	<meta http-equiv="X-UA-Compatible" content="IE-Edge"/>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+	<meta name="description" content="Descrição do meu Site"/>
+	<meta name="keywords" content="palavras,chave,separadas,por,virgula"/>
+	<meta name="author" content="Alcenir da Costa"/>
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="menu-topo">
+				<div class="logo"><a href="#">Logomarca</a></div><!--logo-->
+				<div class="clear"></div><!--clear-->
+				<nav class="desktop">
+					<ul>
+						<li><a href="">Sobre</a></li>
+						<li><a href="">Serviços</a></li>
+						<li><a href="">Contato</a></li>
+						<li class="btn-nav">Quero um orçamento</li>
+					</ul>
+				</nav><!--desktop-->
+				<h3><i class="fas fa-bars"></i></h3>
+				<nav class="mobile">
+					<h3><i class="fas fa-bars"></i></h3>
+					<ul>
+						<li><a href="">Sobre</a></li>
+						<li><a href="">Serviços</a></li>
+						<li><a href="">Contato</a></li>
+						<li class="btn-nav">Quero um orçamento</li>
+					</ul>
+				</nav><!--mobile-->
+				<div class="clear"></div><!--clear-->
+			</div><!--menu-topo-->
+			<div class="chamada-header">
+				<h2>Você vendendo mais<br/> com automação web</h2>
+				<p>Nós criamos soluções de marketing para sua empresa vender mais, da melhor forma!</p>
+				<form>
+					<input type="email" name="email" placeholder="E-mail..." required/>
+					<input type="submit" name="acao" value="Quero saber mais!"/>
+				</form>
+			</div><!--chamada-header-->
+		</div><!--container-->
+	</header>
+	<section class="descricao-danki">
+		<div class="container">
+			<p>Com anos de experiência em desenvolvimento web e automação de marketing, a <b>Nossa Empresa</b> traz soluções que ajudam o seu time a vender mais tanto em segmentos B2C quanto B28.</p>
+		</div><!--container-->
+	</section><!--descricao-danki-->
+	<section class="descricao-danki2">
+		<div class="container">
+			<div class="texto-descricao">
+				<h3>Nós somos uma agência digital. Trabalhamos seu posicionamento, conteúdo e estética, sempre focado em conversão.</h3>
+				<p>Lorem ipsum veniam est consequat incididunt aliquip ad sed dolor veniam dolore dolore in dolor enim sint velit id irure cupidatat do voluptate deserunt ut incididunt dolor do laboris amet in sunt ullamco aute qui veniam cupidatat enim deserunt.</p>
+			</div>
+			<img alt="Trabalho Danki" title="Trabalho Danki" src="_images/mockup1.jpg"/>
+			<div class="clear"></div><!--clear-->
+		</div><!--container-->
+	</section><!--descricao-danki2-->
+	<section class="descricao-danki3">
+		<div class="container">
+			<div class="box-depoimento">
+				<p>"Lorem ipsum magna labore mollit veniam deserunt pariatur in tempor amet non occaecat labore ea incididunt nisi aute amet incididunt sunt dolor excepteur amet officia."</p><br/>
+				<p><b>Dolore laborum elit in fugiat.</b></p>
+			</div><!--box-depoimento-->
+		</div><!--container-->
+	</section><!--descricao-danki3-->
+	<section class="servicos">
+		<div class="container">
+			<div class="title-servicos">
+				<h3>Como podemos ajudar sua empresa<br ?/><i class="fas fa-angle-down"></i></h3>
+			</div><!--title-servicos-->
+			<div class="flex-servicos">
+				<div class="box-servico">
+					<img src="_images/img1.jpg"/>
+					<div class="box-servico-wraper">
+						<h2>Lorem Ipsun</h2>
+						<p>Consequat ut adipisicing ut commodo dolore reprehenderit aliqua et non dolor.</p>
+						<a href="#">Orçamento</a>
+					</div><!--box-servico-wraper-->
+				</div><!--box-servico-->
+				<div class="box-servico">
+					<img src="_images/img2.jpg"/>
+					<div class="box-servico-wraper">
+						<h2>Lorem Ipsun</h2>
+						<p>Consequat ut adipisicing ut commodo dolore reprehenderit aliqua et non dolor.</p>
+						<a href="#">Orçamento</a>
+					</div><!--box-servico-wraper-->
+				</div><!--box-servico-->
+				<div class="box-servico">
+					<img src="_images/img3.jpg"/>
+					<div class="box-servico-wraper">
+						<h2>Lorem Ipsun</h2>
+						<p>Consequat ut adipisicing ut commodo dolore reprehenderit aliqua et non dolor.</p>
+						<a href="#">Orçamento</a>
+					</div><!--box-servico-wraper-->
+				</div><!--box-servico-->
+				<div class="box-servico">
+					<img src="_images/img4.jpg"/>
+					<div class="box-servico-wraper">
+						<h2>Lorem Ipsun</h2>
+						<p>Consequat ut adipisicing ut commodo dolore reprehenderit aliqua et non dolor.</p>
+						<a href="#">Orçamento</a>
+					</div><!--box-servico-wraper-->
+				</div><!--box-servico-->
+				<div class="box-servico">
+					<img src="_images/img5.jpg"/>
+					<div class="box-servico-wraper">
+						<h2>Lorem Ipsun</h2>
+						<p>Consequat ut adipisicing ut commodo dolore reprehenderit aliqua et non dolor.</p>
+						<a href="#">Orçamento</a>
+					</div><!--box-servico-wraper-->
+				</div><!--box-servico-->
+				<div class="box-servico">
+					<img src="_images/img6.jpg"/>
+					<div class="box-servico-wraper">
+						<h2>Lorem Ipsun</h2>
+						<p>Consequat ut adipisicing ut commodo dolore reprehenderit aliqua et non dolor.</p>
+						<a href="#">Orçamento</a>
+					</div><!--box-servico-wraper-->
+				</div><!--box-servico-->
+			</div><!--flex-servicos-->
+		</div><!--coontainer-->
+	</section><!--servicos-->
+	<section class="portfolio">
+		<div class="container">
+			<h2>Algumas empresas que tivemos a oportunidade de atuar...</h2>
+			<div class="empresas">
+				<div class="empresas-single">
+					<img src="_images/bianchini.png"/>
+				</div><!--empresas-single-->
+				<div class="empresas-single">
+					<img src="_images/rancho.png"/>
+				</div><!--empresas-single-->
+				<div class="empresas-single">
+					<img src="_images/hoepcke.png"/>
+				</div><!--empresas-single-->
+				<div class="empresas-single">
+					<img src="_images/monguilhott.png"/>
+				</div><!--empresas-single-->
+			</div><!--empresas-->
+		</div><!--container-->
+	</section><!--portfolio-->
+	<section class="formulario">
+		<div class="container">
+			<h2>Um orçamento ou alguma dúvida?</h2>
+			<form>
+				<input type="text" name="nome" placeholder="Nome..." required/>
+				<input type="text" name="email" placeholder="E-mail..." required/>
+				<input type="text" name ="telefone" placeholder="Telefone..." required/>
+				<textarea name="menagem" placeholder="Deixe-nos sua mensagem!"></textarea>
+				<input type="submit" name="acao" value="Enviar"/>
+			</form>
+		</div><!--container-->
+	</section><!--formulario-->
+	<footer>
+		<div class="container">
+			<div class="box-footer">
+				<h2>Endereço</h2>
+				<p>Lorem ipsum commodo exercitation tempor aliquip quis aliquip dolore deserunt est.</p>
+			</div><!--boox-footer-->
+			<div class="box-footer">
+				<h2>Telefones</h2>
+				<p>(12)1234-5678<br/>(12)8765-4321<br/>(12)2341-5467</p>
+			</div><!--boox-footer-->
+			<div class="box-footer">
+				<h2>Receba nossas novidades!</h2>
+				<form>
+					<input type="text" name="email" placeholder="Insira seu E-mail" /><button type="submit"><i class="fas fa-paper-plane"></i></button>
+				</form>
+			</div><!--boox-footer-->
+			<div class="box-footer">
+				<h2>Siga-nos em nossas redes sociais</h2>
+				<i style="color: #31475e;font-size: 22px;margin-left: 10px;" class="fab fa-facebook"></i>
+				<i style="color: #31475e;font-size: 22px;margin-left: 10px;" class="fab fa-twitter"></i>
+				<i style="color: #31475e;font-size: 22px;margin-left: 10px;" class="fab fa-instagram"></i>
+			</div><!--box-footer-->
+			<div class="clear"></div><!--clear-->
+		</div><!--container-->
+	</footer>
+	<script src="_js/jquery.js"></script>
+	<script src="_js/all.min.js"></script>
+	<script>
+		$('nav.mobile h3').click(function(){
+			$('nav.mobile').find('ul').fadeToggle();
+		});
+	</script>
+</body>
+</html>
